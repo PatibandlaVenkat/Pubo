ee143dd8c0e76e39c4bdccf32c138c5c8c6b7906
prashanth30-n <prashanthnakerikanti@gmail.com>
2026-02-21 22:34:38 +0530

    clerk authentication added and registered first user

--- frontend/Pubo/app/_layout.tsx
+++ frontend/Pubo/app/_layout.tsx
@@ -2,17 +2,21 @@ import { ThemeProvider } from "@/hooks/useTheme";
 import { Stack } from "expo-router";
 import { View } from "react-native";
 import FloatingPenButton from "@/components/FloatingPenButton";
-
+import {ClerkProvider} from '@clerk/clerk-expo'
+import{tokenCache} from '@clerk/clerk-expo/token-cache'
 export default function RootLayout() {
   return(
     <ThemeProvider>
-      <View style={{flex:1}}>
+      <ClerkProvider tokenCache={tokenCache}>
+<View style={{flex:1}}>
          <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false}} />
+      <Stack.Screen name="(home)" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
     </Stack>
     <FloatingPenButton/>
       </View>
-   
+      </ClerkProvider>
     </ThemeProvider>
   )
 }
